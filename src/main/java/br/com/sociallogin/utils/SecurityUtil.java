959b3cbfc69be5930c81aec0f4b0bbb0d1ee291a
rafaelferreiram <rafah.26ferreira@hotmail.com>
2020-02-16 23:30:48 -0300

    commit

---
+++ src/main/java/br/com/sociallogin/utils/SecurityUtil.java
@@ -0,0 +1,25 @@
+package br.com.sociallogin.utils;
+
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.social.security.SocialUserDetails;
+
+import br.com.sociallogin.entity.AppUser;
+import br.com.sociallogin.social.SocialUserDetailsImpl;
+
+public class SecurityUtil {
+
+    // Auto Login.
+    public static void logInUser(AppUser user, List<String> roleNames) {
+
+        SocialUserDetails userDetails = new SocialUserDetailsImpl(user, roleNames);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+}
